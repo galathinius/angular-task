9b0cb0caaadd3fb7189441a8e0ee8f8e3a406f3f
Plesca <aplesca@techteam.com>
2020-08-19 17:39:42 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './appComponent/app.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
-  declarations: [AppComponent, ListsComponent, MoviesComponent],
+  declarations: [AppComponent, ListsComponent, MoviesComponent, AuthComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
