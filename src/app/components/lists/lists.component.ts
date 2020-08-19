b37ce1d125096ab3f38cf57e683886ba9ae8aaab
Plesca <aplesca@techteam.com>
2020-08-19 15:02:46 +0300

    fix: corrected comments

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies/movies.service';
 import { Subscription } from 'rxjs';
 
+enum Categories {
+  nowPlaying = 'now_playing',
+  popular = 'popular',
+  upcoming = 'upcoming',
+}
+
+type movieResponse = {
+  posterPath: string;
+};
+
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
@@ -9,19 +19,20 @@ import { Subscription } from 'rxjs';
 })
 export class ListsComponent implements OnInit {
   @Input('category')
-  category: string = '';
+  category: Categories;
   constructor(private apiService: MoviesService) {}
 
-  Items;
-
+  Items: movieResponse[];
   subs: Subscription;
 
   ngOnInit(): void {
-    this.subs = this.apiService
-      .getData(this.category)
-      .subscribe((resp: any) => {
-        this.Items = resp;
-      });
-    // this.subs.unsubscribe();
+    this.subs = this.apiService.getMovies(this.category).subscribe((resp) => {
+      this.Items = resp;
+      // console.log(resp);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
